a3ba53ab7d077e5c0af5a068e8ff43e3466dcf37
Mary Taylor <marytaylor@users.noreply.github.com>
2020-07-27 15:08:30 -0400

    Create topic_identification.py

---
+++ topic_identification.py
@@ -0,0 +1,46 @@
+# Bag of Words - basic method for finding topics in text
+# first tokenize and then count all tokens
+
+# ------------------------------------------
+
+# Import Counter
+from collections import Counter
+
+# Tokenize the article: tokens
+tokens = word_tokenize(article)
+
+# Convert the tokens into lowercase: lower_tokens
+lower_tokens = [t.lower() for t in tokens]
+
+# Create a Counter with the lowercase tokens: bow_simple
+bow_simple = Counter(lower_tokens)
+
+# Print the 10 most common tokens
+print(bow_simple.most_common(10))
+
+# --------------------------------------------
+# Useful editing: stemming, lemmitization, lowercase, removing puncutation, removing stop words
+
+# Import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer
+
+# Retain alphabetic words: alpha_only
+alpha_only = [t for t in lower_tokens if t.isalpha()]
+
+# Remove all stop words: no_stops
+no_stops = [t for t in alpha_only if t not in english_stops]
+
+# Instantiate the WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+
+# Lemmatize all tokens into a new list: lemmatized
+lemmatized = [wordnet_lemmatizer.lemmatize(t) for t in no_stops]
+
+# Create the bag-of-words: bow
+bow = Counter(lemmatized)
+
+# Print the 10 most common tokens
+print(bow.most_common(10))
+
+# ---------------------------------------------
+
