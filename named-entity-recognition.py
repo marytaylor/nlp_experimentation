a6550b7fd6b25e8b1f5214af33d5e87c73566591
Mary Taylor <marytaylor@users.noreply.github.com>
2020-07-29 22:48:45 -0400

    Update named-entity-recognition.py

--- named-entity-recognition.py
+++ named-entity-recognition.py
@@ -30,3 +30,11 @@ for sent in chunked_sentences:
 # Create a list from the dictionary keys for the chart labels: labels
 labels = list(ner_categories.keys())
 
+# Create a list of the values: values
+values = [ner_categories.get(v) for v in labels]
+
+# Create the pie chart
+plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
+
+# Display the chart
+plt.show()
